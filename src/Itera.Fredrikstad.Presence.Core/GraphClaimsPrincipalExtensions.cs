654ed45b4dcb27cf3463e2b2029cd9fa36f8f5e6
Anders Klund-Hansen <anders.klund.hansen@gmail.com>
2022-06-10 14:56:41 +0200

    Removed unused code

--- src/Itera.Fredrikstad.Presence.Core/GraphClaimsPrincipalExtensions.cs
+++ src/Itera.Fredrikstad.Presence.Core/GraphClaimsPrincipalExtensions.cs
@@ -3,7 +3,6 @@
 
 // <GraphClaimsExtensionsSnippet>
 using System.Security.Claims;
-using Microsoft.Extensions.Identity.Core;
 using Microsoft.Graph;
 
 namespace Itera.Fredrikstad.Presence.Core
@@ -13,8 +12,6 @@ namespace Itera.Fredrikstad.Presence.Core
         public const string DisplayName = "graph_name";
         public const string Email = "graph_email";
         public const string Photo = "graph_photo";
-        public const string TimeZone = "graph_timezone";
-        public const string TimeFormat = "graph_timeformat";
     }
 
     // Helper methods to access Graph user data stored in
@@ -36,30 +33,15 @@ namespace Itera.Fredrikstad.Presence.Core
             return claimsPrincipal.FindFirstValue(GraphClaimTypes.Photo);
         }
 
-        public static string GetUserGraphTimeZone(this ClaimsPrincipal claimsPrincipal)
-        {
-            return claimsPrincipal.FindFirstValue(GraphClaimTypes.TimeZone);
-        }
-
-        public static string GetUserGraphTimeFormat(this ClaimsPrincipal claimsPrincipal)
-        {
-            return claimsPrincipal.FindFirstValue(GraphClaimTypes.TimeFormat);
-        }
-
         public static void AddUserGraphInfo(this ClaimsPrincipal claimsPrincipal, User user)
         {
             var identity = claimsPrincipal.Identity as ClaimsIdentity;
 
-            identity.AddClaim(
+            identity?.AddClaim(
                 new Claim(GraphClaimTypes.DisplayName, user.DisplayName));
-            identity.AddClaim(
+            identity?.AddClaim(
                 new Claim(GraphClaimTypes.Email,
                     user.Mail ?? user.UserPrincipalName));
-            // identity.AddClaim(
-            //     new Claim(GraphClaimTypes.TimeZone,
-            //         user.MailboxSettings.TimeZone));
-            // identity.AddClaim(
-            //     new Claim(GraphClaimTypes.TimeFormat, user.MailboxSettings.TimeFormat));
         }
 
         public static void AddUserGraphPhoto(this ClaimsPrincipal claimsPrincipal, Stream photoStream)
@@ -69,7 +51,7 @@ namespace Itera.Fredrikstad.Presence.Core
             if (photoStream == null)
             {
                 // Add the default profile photo
-                identity.AddClaim(
+                identity?.AddClaim(
                     new Claim(GraphClaimTypes.Photo, "/img/no-profile-photo.png"));
                 return;
             }
@@ -83,7 +65,7 @@ namespace Itera.Fredrikstad.Presence.Core
             // Generate a date URI for the photo
             var photoUrl = $"data:image/png;base64,{Convert.ToBase64String(photoBytes)}";
 
-            identity.AddClaim(
+            identity?.AddClaim(
                 new Claim(GraphClaimTypes.Photo, photoUrl));
         }
     }
